73b4d3d71c12f668c0dcb005d2bb1a67519c9bc6
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-18 23:45:43 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   
   def change 
-    change_column :students, :birthdate, :datetime
+    change_column :students, :birthdate, :datetime 
+  end
   
 end
