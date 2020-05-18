78e258ddf61b92a6e8a6b8f5f3bd877be34352b1
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-18 23:42:48 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,7 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
+  
   def change
-    add_column :artists, :favorite_food, :string
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string 
   end
 end
